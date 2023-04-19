b37d44b55e8aaeab7b1a5613d292b3460542e450
“Alex <korolenkosasha644@gmail.com>
2023-04-19 12:19:10 +0300

    Add new Project

---
+++ WebApplication1/Data/Entities/WeatherNow.cs
@@ -0,0 +1,8 @@
+﻿namespace WebApplication1.Data.Entities;
+
+public class WeatherNow
+{
+    public int Id { get; set; }
+    public double Temperature { get; set; }
+    public DateTime Date { get; set; }
+}
