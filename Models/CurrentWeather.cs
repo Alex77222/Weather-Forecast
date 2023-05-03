be17c4adba9f336ebf01cc7bbf3499fb977ac23d
“Alex <korolenkosasha644@gmail.com>
2023-05-03 21:53:03 +0300

    Add CurrentWeather

---
+++ Models/CurrentWeather.cs
@@ -0,0 +1,7 @@
+namespace WebApplication1.Models;
+
+public class CurrentWeather
+{
+    public double temperature { get; set; }
+    public DateTime time { get; set; }
+}
--- Models/WeatherApi.cs
+++ Models/WeatherApi.cs
@@ -3,5 +3,6 @@ namespace WebApplication1.Models;
 public class WeatherApi
 {
     public Hourly?  Hourly { get; set; }
-    
+
+    public CurrentWeather? current_Weather { get; set; }
 }
